32faca77b4ef728da8274bccbd650eb5080d6894
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-11-23 21:47:30 -0500

    feat: Do not depends on package.json and use new repositoryUrl
    BREAKING CHANGE: Require semantic-release version that passed the `repositoryUrl` option

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const url = require('url');
 const getStream = require('get-stream');
 const intoStream = require('into-stream');
-const hostedGitInfo = require('hosted-git-info');
+const gitUrlParse = require('git-url-parse');
 const conventionalCommitsParser = require('conventional-commits-parser').sync;
 const conventionalChangelogWriter = require('conventional-changelog-writer');
 const debug = require('debug')('semantic-release:release-notes-generator');
@@ -16,32 +16,32 @@ const loadChangelogConfig = require('./lib/load-changelog-config');
  * @param {Object} pluginConfig.parserOpts additional `conventional-changelog-parser` options that will overwrite ones loaded by `preset` or `config`.
  * @param {Object} pluginConfig.writerOpts additional `conventional-changelog-writer` options that will overwrite ones loaded by `preset` or `config`.
  * @param {Object} options semantic-release options
- * @param {Object} options.pkg normalized `package.json`
  * @param {Array<Object>} options.commits array of commits, each containing `hash` and `message`
  * @param {Object>} options.lastRelease last release with `gitHead` corresponding to the commit hash used to make the last release and `gitTag` corresponding to the git tag associated with `gitHead`
  * @param {Object>} options.nextRelease next release with `gitHead` corresponding to the commit hash used to make the  release, the release `version` and `gitTag` corresponding to the git tag associated with `gitHead`
+ * @param {Object} options.options.repositoryUrl git repository URL
  */
-async function releaseNotesGenerator(pluginConfig, {pkg, commits, lastRelease, nextRelease}) {
+async function releaseNotesGenerator(pluginConfig, {commits, lastRelease, nextRelease, options: {repositoryUrl}}) {
   const {parserOpts, writerOpts} = await loadChangelogConfig(pluginConfig);
   commits = commits.map(rawCommit =>
     Object.assign(rawCommit, conventionalCommitsParser(rawCommit.message, parserOpts))
   );
-  const {default: protocol, domain: host, project: repository, user: owner} = hostedGitInfo.fromUrl(pkg.repository.url);
+  const {resource: hostname, port, name: repository, owner} = gitUrlParse(repositoryUrl);
+  const protocol = url.parse(repositoryUrl).protocol || 'https';
   const previousTag = lastRelease.gitTag || lastRelease.gitHead;
   const currentTag = nextRelease.gitTag || nextRelease.gitHead;
   const context = {
     version: nextRelease.version,
-    host: url.format({protocol, host}),
+    host: url.format({protocol, hostname, port}),
     owner,
     repository,
     previousTag,
     currentTag,
     linkCompare: currentTag && previousTag,
-    packageData: pkg,
   };
 
   debug('version: %o', nextRelease.version);
-  debug('host: %o', host);
+  debug('host: %o', hostname);
   debug('owner: %o', owner);
   debug('repository: %o', repository);
   debug('previousTag: %o', previousTag);
